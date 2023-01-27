b9bdd75feee21d2f9097db683e07c2e1ed60ca5d
PEGEOT_Sam <sam.pegeot@etu.minesparis.psl.eu>
2023-01-27 14:50:54 +0100

    big changes

--- src/Personnage.hpp
+++ src/Personnage.hpp
@@ -3,34 +3,42 @@
 #include <vector>
 #include <string>
 #include <memory>
-#include "objet.hpp"
+// #include "objet.hpp"
 
-class Personnage{
-    virtual int avancer_x(const int pas) = 0;
+class Personnage
+{
+public:
+  virtual int avancer_x(const int pas) = 0;
 
-    virtual int avancer_y(const int pas)=0;
+  virtual int avancer_y(const int pas) = 0;
 
-    virtual void print_sante()=0;
+  virtual void print_sante() = 0;
 
-    virtual void recevoir_objet( const std::shared_ptr<Objet> objet)=0;
+  // virtual void recevoir_objet( const std::shared_ptr<Objet> objet)=0;
 
-    std::vector<int> get_position();
+  std::vector<int> get_position();
 
-    std::string get_symbole();
+  std::string get_symbole();
 
+protected:
+  std::string nom_;
+  int sante_;
+  std::string symbole_;
+  int x_;
+  int y_;
+  int defense_;
 };
 
-class Joueur : public Personnage{
-    int avancer_x(int pas );
-    int avancer_y(int pas);
-    void recevoir_objet(const std::shared_ptr<Objet> objet);
-    void print_sante();
+class Joueur : public Personnage
+{
+public:
+  Joueur(std::string name, int x, int y);
+  int avancer_x(int pas);
+  int avancer_y(int pas);
+  // void recevoir_objet(const std::shared_ptr<Objet> objet);
+  void print_sante();
 };
 
-
-
-
-
-
-
-
+class Ennemi : public Personnage
+{
+};
